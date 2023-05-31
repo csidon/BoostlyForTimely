6095681d2cebb54684f34ab532131e7482aac9dd
Ana Karpova <nastya.karpova123@gmail.com>
2023-06-01 00:18:12 +1200

    UI created for bOwn and bEmp. Message preview dummy data added

--- static/functions.js
+++ static/functions.js
@@ -1,20 +1,34 @@
 function filterTable() {
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var table = document.getElementById("myTable");
-    var rows = table.getElementsByTagName("tr");
+  var checkboxes = document.querySelectorAll('#checkboxContainer input[type="checkbox"]');
+  var table = document.getElementById("myTable");
+  var rows = table.getElementsByTagName("tr");
 
-    for (var i = 1; i < rows.length; i++) {
-      var dayCell = rows[i].getElementsByTagName("td")[0];
-      var day = dayCell.textContent || dayCell.innerText;
-      var shouldShow = false;
+  for (var i = 1; i < rows.length; i++) {
+    var dayCell = rows[i].getElementsByTagName("td")[3];
+    var day = dayCell.textContent || dayCell.innerText;
+    var shouldShow = false;
 
-      for (var j = 0; j < checkboxes.length; j++) {
-        if (checkboxes[j].checked && checkboxes[j].id === "checkbox" + day) {
-          shouldShow = true;
-          break;
-        }
+    for (var j = 0; j < checkboxes.length; j++) {
+      if (checkboxes[j].checked && checkboxes[j].value === day) {
+        shouldShow = true;
+        break;
       }
-
-      rows[i].style.display = shouldShow ? "" : "none";
     }
+ 
+    rows[i].style.display = shouldShow ? "" : "none";
   }
+}
+
+function updateMessagePreview() {
+  var date = document.getElementById("dateInput").value;
+  var time = document.getElementById("timeInput").value;
+  var availabilityLength = document.getElementById("availabilityInput").value;
+
+  var messagePreview = "Hello, I am contacting everyone on my waitlist as " + availabilityLength + " minute slot available on " + date + " starting on " + time;
+  document.getElementById("messagePreview").value = messagePreview;
+}
+
+// Attach event listeners to update preview on field changes
+document.getElementById("dateInput").addEventListener("change", updateMessagePreview);
+document.getElementById("timeInput").addEventListener("change", updateMessagePreview);
+document.getElementById("availabilityInput").addEventListener("input", updateMessagePreview);
